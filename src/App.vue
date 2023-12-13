16ff0e6a52a9e0b4cdc378bb21a42a896d580771
nandar510217 <nandarwin0106@gmail.com>
2023-12-13 21:52:21 +0630

    get post and store post

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,17 @@
   <div class="container">
     <div class="row my-3">
       <div class="col-md-6">
-        <h4>Axios</h4>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci nulla animi nam magnam nisi fuga, consequatur qui dolores assumenda distinctio ducimus, iusto, tempore pariatur non atque aperiam. Ratione, at quisquam!</p>
+        <h4>Post Entry From</h4>
+        <div class="mb-2">
+          <input type="text" v-model="form.title" class="form-control">
+        </div>
+        <div class="mb-2">
+          <input type="text" v-model="form.body" class="form-control">
+        </div>
+        <div class="mb-2">
+          <input type="text" v-model="form.userId" class="form-control">
+        </div>
+        <button class="btn btn-info btn-sm" @click="storePosts">Save</button>
       </div>
       <div class="col-md-6">
         <!-- <button class="btn btn-primary" @click="loadPost">Load Posts</button> -->
@@ -19,9 +28,25 @@
 </template>
 
 <script setup>
-  import {onMounted, ref} from 'vue';
+  import {onMounted, reactive, ref} from 'vue';
 
   const posts = ref([])
+
+  //write
+  const form = reactive({
+    title: null,
+    body: null,
+    userId: null
+  })
+
+  //store posts
+  const storePosts = () => {
+    axios.post('https://jsonplaceholder.typicode.com/posts', form)
+    .then((res) => {
+      posts.value.unshift(res.data)
+    })
+    .catch((err) => console.log(err))
+  }
 
   //get posts
   const getPosts = () => {
@@ -32,12 +57,12 @@
   .catch((err) =>  console.log(err))
   }
   
-  //getPosts နဲ့ ခေါ်လို့ရပေမယ့် onMounted ထဲမှာပိုကောင်း browser ထဲတကယ်ရောက်လာမှအလုပ်လုပ်
+  //getPosts နဲ့ ခေါ်လို့ရပေမယ့် onMounted ထဲမှာရေးတာပိုကောင်း browser ထဲတကယ်ရောက်လာမှအလုပ်လုပ်
   onMounted(() => {
     getPosts();
   })
 
-  
+
 </script>
 
 <style scoped>
